621c1a9a379fe693e23eb9137439c8d3af0a04ae
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-23 12:59:36 -0500

    Update GlobalVariables.gd

--- GlobalVariables.gd
+++ GlobalVariables.gd
@@ -7,13 +7,13 @@ func _ready():
 	generateColors()
 
 func generateColors():
-	var colorsLeft = 5
+	var colorsLeft = 6
 	var rng = RandomNumberGenerator.new()
 	rng.randomize()
 	while colorsLeft > 0:
-		var r = rng.randf_range(0, 1)
-		var g = rng.randf_range(0, 1)
-		var b = rng.randf_range(0, 1)
+		var r = rng.randf_range(0, 0.7)
+		var g = rng.randf_range(0, 0.7)
+		var b = rng.randf_range(0, 0.7)
 		var color = Color(r, g, b)
 		colors.append(color)
 		colorsLeft -= 1
