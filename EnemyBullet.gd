aac2e396074e04eba900de6245e5ce046ff95676
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-23 12:59:32 -0500

    Create EnemyBullet.gd

---
+++ EnemyBullet.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+export (int) var damage
+var Velocity = Vector2()
+
+func start(Velocity_, rotation_, position_):
+	$Sprite.modulate = GlobalVariables.colors[5]
+	position = position_
+	print(rotation_)
+	var direction = Vector2(1, 0).rotated(rotation_)
+	rotation = rotation_
+	Velocity = Velocity_
+
+func _physics_process(delta):
+	position += Velocity
+
+func _on_Lifetime_timeout():
+	explode()
+
+func explode():
+	queue_free()
+
+func _on_Bullet_body_entered(body):
+	if body.name == "Player":# || body.type == StaticBody2D:
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+		explode()
+		
+
