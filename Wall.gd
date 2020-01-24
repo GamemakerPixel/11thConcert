6f2b161044b8b0371ba571ca27cd2ff756b900ca
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-24 17:44:09 -0500

    Update Wall.gd

--- Wall.gd
+++ Wall.gd
@@ -5,3 +5,8 @@ extends StaticBody2D
 func _ready():
 	if GlobalVariables.colors.size() > 0:
 		$Sprite.modulate = GlobalVariables.colors[2]
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if GlobalVariables.colors.size() > 0:
+			$Sprite.modulate = GlobalVariables.colors[2]
