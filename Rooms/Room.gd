37e08e35fb91cf73d3e0ce5f5b4dfc12e87fbc01
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-24 17:44:07 -0500

    Update Room.gd

--- Rooms/Room.gd
+++ Rooms/Room.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-var spawnRoom
+export (bool) var spawnRoom
+var enemySpawnChance = 100
+var powerupSpawnChance = 25
+var enemy = preload("res://11thConcert/Enemy.tscn")
 
 func _ready():
 	if spawnRoom:
 		$EnemySpawns.queue_free()
 		$PowerSpawns.queue_free()
 	else:
-		pass
+		var rng = RandomNumberGenerator.new()
+		rng.randomize()
+		var spawnsLeft = $EnemySpawns.get_child_count()
+		while spawnsLeft > 0:
+			if rng.randi_range(0, 100) <= enemySpawnChance:
+				var e = enemy.instance()
+				add_child(e)
+				e.position = get_node("EnemySpawns").get_node(str(spawnsLeft)).position
+			spawnsLeft -= 1
