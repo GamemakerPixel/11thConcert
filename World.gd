aa7e8638ba9cfa5193e8fdb01e91048819f5b421
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-24 17:44:11 -0500

    Update World.gd

--- World.gd
+++ World.gd
@@ -5,3 +5,8 @@ extends Node2D
 func _ready():
 	if GlobalVariables.colors.size() > 0:
 		VisualServer.set_default_clear_color(GlobalVariables.colors[3])
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		if GlobalVariables.colors.size() > 0:
+			VisualServer.set_default_clear_color(GlobalVariables.colors[3])
