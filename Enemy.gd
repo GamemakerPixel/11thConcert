3d978abf804b09c7b4d8d2dd4eb10cdb762de35c
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-28 15:20:29 -0500

    Update Enemy.gd

--- Enemy.gd
+++ Enemy.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var health = 3
 var damage = 1
-var speed = 10
+var speed = 100
 var readyToFire = false
 var canShoot = false
 var Velocity = Vector2(0, 0)
@@ -17,15 +17,15 @@ func _process(delta):
 	if readyToFire:
 		var playerEnemyDistance = position.distance_to(GlobalVariables.playerPos)
 		if playerEnemyDistance > 192: 
-			Velocity = Vector2(100, 0)
+			Velocity = Vector2(speed, 0)
 		elif playerEnemyDistance < 128:
-			Velocity = Vector2(-200, 0)
+			Velocity = Vector2(-speed*2, 0)
 		else:
 			Velocity = Velocity.linear_interpolate(Vector2(0, 0), 1)
 	look_at(GlobalVariables.playerPos)
 	if readyToFire && canShoot:
 		shoot()
-	move_and_slide(Velocity.rotated(rotation))
+	move_and_slide(Velocity).rotated(rotation) #Related to swinging enemies bug, Velocity doesn't matter, try testing w/ (0, 0)
 	if Input.is_action_just_pressed("ui_accept"):
 		$Sprite.modulate = GlobalVariables.colors[4]
 
