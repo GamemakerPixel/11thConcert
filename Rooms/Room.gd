e345a7870ff784cf1fed9e08b54d8d955bc3373d
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2020-01-28 15:20:51 -0500

    Update Room.gd

--- Rooms/Room.gd
+++ Rooms/Room.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 export (bool) var spawnRoom
-var enemySpawnChance = 50
-var powerupSpawnChance = 25
+var enemySpawnChance = 25
+var powerupSpawnChance = 10
 var enemy = preload("res://11thConcert/Enemy.tscn")
+var power = preload("res://11thConcert/Powerups/1.tscn")
 
 func _ready():
 	if spawnRoom:
@@ -16,6 +17,11 @@ func _ready():
 		while spawnsLeft > 0:
 			if rng.randi_range(0, 100) <= enemySpawnChance:
 				var e = enemy.instance()
-				add_child(e)
-				e.position = get_node("EnemySpawns").get_node(str(spawnsLeft)).position
+				get_node("EnemySpawns/" + str(spawnsLeft)).add_child(e)
 			spawnsLeft -= 1
+		var powerSpawnsLeft = $PowerSpawns.get_child_count()
+		while powerSpawnsLeft > 0:
+			if rng.randi_range(0, 100) <= powerupSpawnChance:
+				var p = power.instance()
+				get_node("PowerSpawns/" + str(powerSpawnsLeft)).add_child(p)
+			powerSpawnsLeft -= 1
